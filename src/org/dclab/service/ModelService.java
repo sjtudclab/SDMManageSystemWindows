bc931497efecd972d838dbe1d9ea3485e13fe081
alvis <347390728@qq.com>
2016-10-27 15:34:16 +0800

    clean DM file after export

--- src/org/dclab/service/ModelService.java
+++ src/org/dclab/service/ModelService.java
@@ -174,6 +174,7 @@ public class ModelService {
 			} catch (IOException ex) {
 				ex.printStackTrace();
 			}
+			deleteDir(file);
 		} else {
 			String filename1 = dir + File.separator + "DM.zip";
 			InputStream iStream = new FileInputStream(new File(filename1));
@@ -188,4 +189,19 @@ public class ModelService {
 		System.out.println("文件列表");
 		return myModelMapperI.getProjectlist(username);
 	}
+	
+    private static boolean deleteDir(File dir) {
+        if (dir.isDirectory()) {
+            String[] children = dir.list();
+            //递归删除目录中的子目录下
+            for (int i=0; i<children.length; i++) {
+                boolean success = deleteDir(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+        }
+        // 目录此时为空，可以删除
+        return dir.delete();
+    }
 }
